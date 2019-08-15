1c4280dcee2584b096651ff2e36ae52cefe01905
yizeruier1 <154539188@qq.com>
2019-08-15 10:17:26 +0800

    添加分号单引号改为双引号

--- exam.js
+++ exam.js
@@ -1,56 +1,61 @@
-// import crypto from 'crypto'
-// import axios from 'axios'
-const crypto = require('crypto')
-const axios = require('axios')
-
-const url = 'https://dm.aliyuncs.com/'
+const crypto = require('crypto');
+const axios = require('axios');
+const url = "https://dm.aliyuncs.com/";
 // 动态配置 必要参数
 const requireParam = [
-    'accessKeyID',
-    'accessKeySecret',
-    'accountName',
-    'single--toAddress',
-    'batch--templateName',
-    'batch--receiversName'
-]
+    "accessKeyID",
+    "accessKeySecret",
+    "accountName",
+    "single--toAddress",
+    "batch--templateName",
+    "batch--receiversName"
+];
 const exam = (config, cb) => {
-    if (!config || JSON.stringify(config) === "{}") return cb('config required')
-    const nonce = Date.now()
-    const date = new Date()
-    const errorMsg = []
+    if (!config || JSON.stringify(config) === "{}"){
+        return cb("config required");
+    }
+    const nonce = Date.now();
+    const date = new Date();
+    const errorMsg = [];
     // 缺少参数 处理
     if(!config.action){
-        cb('error action')
+        cb("error action");
     }else{
-        requireParam.forEach(item => {
-            if (item.indexOf('--') === -1) {
-                if (!config[item]) errorMsg.push(`${item} required`)
+        requireParam.forEach((item) => {
+            if (item.indexOf("--") === -1) {
+                if (!config[item]){
+                    errorMsg.push(`${item} required`);
+                }
             } else {
-                let paramArr = item.split('--')
-                if (config.action === paramArr[0] && !config[paramArr[1]]) errorMsg.push(`${paramArr[1]} required`)
+                let paramArr = item.split("--");
+                if (config.action === paramArr[0] && !config[paramArr[1]]){
+                    errorMsg.push(`${paramArr[1]} required`);
+                }
             }
         })
         if (errorMsg.length) {
-            return cb(errorMsg.join(','));
+            return cb(errorMsg.join(","));
         }
     }
     // 初始化参数
     let param = {
         AccessKeyId: config.accessKeyID,
-        Format: 'JSON',
+        Format: "JSON",
         Action: config.action,
         AccountName: config.accountName,
         ReplyToAddress: !!config.replyToAddress,
-        AddressType: typeof config.addressType === 'undefined' ? 0 : config.addressType,
+        AddressType: typeof config.addressType === "undefined" ? 0 : config.addressType,
         ToAddress: config.toAddress,
-        SignatureMethod: 'HMAC-SHA1',
+        TemplateName: config.templateName,
+        ReceiversName: config.receiversName,
+        SignatureMethod: "HMAC-SHA1",
         SignatureNonce: nonce,
-        SignatureVersion: '1.0',
+        SignatureVersion: "1.0",
         TemplateCode: config.templateCode,
         Timestamp: date.toISOString(),
-        Version: '2015-11-23'
-    }
-    if (config.action === 'single'){
+        Version: "2015-11-23"
+    };
+    if (config.action === "single"){
         if (config.fromAlias) {
             param.FromAlias = config.fromAlias;
         }
@@ -64,35 +69,35 @@ const exam = (config, cb) => {
             param.TextBody = config.textBody;
         }
     }
-    if (config.action === 'batch' && config.tagName) {
-        param.TagName = config.tagName
+    if (config.action === "batch" && config.tagName) {
+        param.TagName = config.tagName;
     }
 
     axios({
-        method: 'post',
+        method: "post",
         url: url,
         data: param,
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            "Content-Type": "application/x-www-form-urlencoded"
         },
         transformRequest: data => {
             // 参数 处理
-            let signStr = []
-            let reqBody = []
+            let signStr = [];
+            let reqBody = [];
             Object.keys(data).forEach(i => {
-                signStr.push(encodeURIComponent(i) + '=' + encodeURIComponent(data[i]))
-                reqBody.push(i + '=' + data[i])
+                signStr.push(encodeURIComponent(i) + "=" + encodeURIComponent(data[i]));
+                reqBody.push(i + "=" + data[i]);
             })
-            signStr = 'POST&%2F&' + encodeURIComponent(signStr.sort().join('&'))
-            const sign = crypto.createHmac("sha1", config.accessKeySecret + '&').update(signStr).digest('base64')
-            const signature = encodeURIComponent(sign)
-            reqBody.unshift('Signature=' + signature)
-            return reqBody.join('&')
+            signStr = "POST&%2F&" + encodeURIComponent(signStr.sort().join("&"));
+            const sign = crypto.createHmac("sha1", config.accessKeySecret + "&").update(signStr).digest("base64");
+            const signature = encodeURIComponent(sign);
+            reqBody.unshift("Signature=" + signature);
+            return reqBody.join("&");
         }
     }).then(res => {
-        cb(res.data)
+        cb(res.data);
     }).catch(err => {
-        cb(err.data, err.config)
+        cb(err.data, err.config);
     })
 }
 module.exports = exam
--- exam.test.js
+++ exam.test.js
@@ -1,34 +1,34 @@
-const exam = require('./exam')
+const exam = require("./exam");
 
 const callback = (msg, data) => {
-    console.log(msg)
+    console.log(msg);
 }
 
-test('exam config = {}', () => {
-    expect(exam({}, callback)).toBe(callback('config required'))
+test("exam config = {}", () => {
+    expect(exam({}, callback)).toBe(callback("config required"));
 })
 
 
 const param = {
-    accessKeyID : 'accessKeyID',
-    accessKeySecret: 'accessKeySecret',
-    accountName: 'accountName',
-    toAddress: 'single--toAddress',
-    templateName: 'batch--templateName',
-    receiversName: 'batch--receiversName'
+    accessKeyID : "accessKeyID",
+    accessKeySecret: "accessKeySecret",
+    accountName: "accountName",
+    toAddress: "single--toAddress",
+    templateName: "batch--templateName",
+    receiversName: "batch--receiversName"
 }
-test('exam config without action', () => {
-    expect(exam(param, callback)).toBe(callback('error action'))
+test("exam config without action", () => {
+    expect(exam(param, callback)).toBe(callback("error action"));
 })
 
 const param1 = {
-    accessKeySecret: 'accessKeySecret',
-    accountName: 'accountName',
-    toAddress: 'single--toAddress',
-    templateName: 'batch--templateName',
-    receiversName: 'batch--receiversName',
-    action: 'batch'
+    accessKeySecret: "accessKeySecret",
+    accountName: "accountName",
+    toAddress: "single--toAddress",
+    templateName: "batch--templateName",
+    receiversName: "batch--receiversName",
+    action: "batch"
 }
-test('exam config without action', () => {
-    expect(exam(param1, callback)).toBe(callback('accessKeyID required'))
+test("exam config without action", () => {
+    expect(exam(param1, callback)).toBe(callback("accessKeyID required"));
 })
